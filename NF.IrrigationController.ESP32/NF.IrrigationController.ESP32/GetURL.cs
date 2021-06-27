b44de5adea064d562ff05185a2cc863d806cc93a
dweaver309 <dweaver309@gmail.com>
2021-06-27 09:07:31 -0500

    Updated files

---
+++ NF.IrrigationController.ESP32/NF.IrrigationController.ESP32/GetURL.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Text;
+
+namespace NF.IrrigationController.ESP32
+{
+   public class GetURL
+    {
+        public static string DecodeURLString(string strURL )
+        {
+            if (InString(strURL, "favicon.ico"))
+                return "HTTP/1.0 404\r\n";
+
+            
+            // Default page in URL
+            if (InString(strURL, "default.html"))
+            {
+                // Get statement returned
+                if (InString(strURL, "?"))
+                {
+                    // Get parameters array
+                    Param[] mParm = Param.DecryptParam(strURL);
+
+                    if (mParm[0].Name.Length > 1)
+                    {
+                       string Status = ProcessDefaultParam(mParm[0].Name);
+
+                        if (Status == "Settings")
+                        {
+                            return WebPages.SettingsPage() ;
+
+                        }
+                        else
+                        {
+                            return WebPages.StatusPage(Status);
+                        }
+                    }
+                }
+                else
+                {
+                    return WebPages.DefaultPage();
+                }
+
+                if (InString(strURL, "settings.html"))
+                {
+                    if (InString(strURL, "?"))
+                    {
+
+                        //ToDo
+                        Param[] mParm = Param.DecryptParam(strURL);
+
+                        ProcessSettingsParam(mParm);
+
+                        return WebPages.StatusPage("Under Construction");
+                    }
+                }
+              
+               
+
+            }
+            return WebPages.DefaultPage();
+        }
+
+        private static void ProcessSettingsParam(Param[] mParam)
+        {
+
+            if (mParam[0].Name == "Zone1MaxTime")
+            {
+
+            }
+
+        }
+
+            private static string ProcessDefaultParam(string ParamName)
+        {
+            if (InString(ParamName, "Off") == true)
+            {
+                ZoneTimer.CurrentZoneOff = true;
+
+                if (ZoneTimer.ProgramActivated)
+                {
+                    ZoneTimer.ProgramActivated = false;
+
+                    return "Program Off";
+                }
+
+                return "Zone Off";
+            }
+
+            switch (ParamName)
+            {
+                case "Zone1On":
+                    {
+                        ZoneTimer.StartTimer(ZoneTimer.Zone1, ZoneTimer.Zone1MaxTime);
+
+                        return "Zone 1 On";
+                    }
+
+                case "Zone2On":
+                    {
+                        ZoneTimer.StartTimer(ZoneTimer.Zone2, ZoneTimer.Zone2MaxTime);
+
+                        return "Zone 2 On";
+                    }
+
+                case "Zone3On":
+                    {
+                        ZoneTimer.StartTimer(ZoneTimer.Zone3, ZoneTimer.Zone3MaxTime);
+
+                        return "Zone 3 On";
+                    }
+
+                case "Zone4On":
+                    {
+                        ZoneTimer.StartTimer(ZoneTimer.Zone4, ZoneTimer.Zone4MaxTime);
+
+                        return "Zone 4 On";
+                    }
+                         
+
+                case "ProgramOn":
+                    {
+                        ZoneTimer.ProgramActivated = true;
+
+                        return "Program Started";
+                    }
+
+                case "Settings":
+                    {
+                        return "Settings";
+                    }
+            }
+
+
+            return "Error";
+        }
+
+            
+
+        /// <summary>
+        ///     ''' Displays string to console when Debugging is set to True
+        ///     ''' </summary>
+        private static void PrintData(string Str)
+        {
+            Console.WriteLine(Str);
+        }
+
+        /// <summary>
+        ///     ''' Returns True if a string is part of another
+        ///     ''' </summary>
+        private static bool InString(string String1, string StringToFind)
+        {
+            try
+            {
+                if (String1 == string.Empty)
+                    return false;
+
+                if (StringToFind == string.Empty)
+                    return false;
+
+                String1 = String1.ToUpper();
+
+                StringToFind = StringToFind.ToUpper();
+
+                if (String1.IndexOf(StringToFind) == -1)
+                    return false;
+                else
+                    return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+
+
+    
+    }
+
---
+++ NF.IrrigationController.ESP32/NF.IrrigationController.ESP32/ParseRequest.cs
@@ -0,0 +1,122 @@
+﻿// This Class module was derived from code written by Laurent Ellerbach for .netmf
+
+
+// This Class module was written by Laurent Ellerbach in C#
+// https://blogs.msdn.microsoft.com/laurelle/
+// David Weaver translated it to VB with minor changes
+
+/// <summary>
+
+/// ''' Get the Parameters from URL
+
+/// ''' </summary>
+using System;
+
+namespace NF.IrrigationController.ESP32
+{
+public class Param
+{
+    public const char ParamSeparator = '&';
+
+    public const char ParamStart = '?';
+
+    public const char ParamEqual = '=';
+
+    public string Name
+    {
+        get
+        {
+            return m_Name;
+        }
+
+        set
+        {
+            m_Name = value;
+        }
+    }
+
+    private string m_Name;
+
+    public string Value
+    {
+        get
+        {
+            return m_Value;
+        }
+
+        set
+        {
+            m_Value = value;
+        }
+    }
+
+    private string m_Value;
+
+    public static Param[] DecryptParam(String Parameters)
+    {
+        Param[] retParams = null;
+
+        int i = Parameters.IndexOf(ParamStart);
+
+        int j = i;
+
+        int k;
+
+        if (i >= 0)
+        {
+
+            // look at the number of = and ;
+            while ((i < Parameters.Length) || (i == -1))
+            {
+                j = Parameters.IndexOf(ParamEqual, i);
+
+                if (j > i)
+                {
+
+                    // first param!
+                    if (retParams == null)
+                    {
+                        retParams = new Param[1];
+
+                        retParams[0] = new Param();
+                    }
+                    else
+                    {
+                        Param[] rettempParams = new Param[retParams.Length + 1];
+
+                        retParams.CopyTo(rettempParams, 0);
+
+                        rettempParams[rettempParams.Length - 1] = new Param();
+
+                        retParams = new Param[rettempParams.Length - 1 + 1];
+
+                        rettempParams.CopyTo(retParams, 0);
+                    }
+
+                    k = Parameters.IndexOf(ParamSeparator, j);
+
+                    retParams[retParams.Length - 1].Name = Parameters.Substring(i + 1, j - i - 1);
+
+
+                    if (k == j)
+                        retParams[retParams.Length - 1].Value = "";
+                    else if (k > j)
+                        retParams[retParams.Length - 1].Value = Parameters.Substring(j + 1, k - j - 1);
+                    else
+                        retParams[retParams.Length - 1].Value = Parameters.Substring(j + 1, Parameters.Length - j - 1);
+
+                    if (k > 0)
+                        i = Parameters.IndexOf(ParamSeparator, k);
+                    else
+                        i = Parameters.Length;
+                }
+                else
+                    i = -1;
+            }
+        }
+
+        return retParams;
+    }
+}
+
+}
